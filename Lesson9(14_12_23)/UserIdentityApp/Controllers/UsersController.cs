49ccc8fb8d4a8471b9c4689b0cc7695885364d48
Emre <esyolal@gmail.com>
2023-12-14 22:45:05 +0300

    Added Lesson9(Identity Project)

---
+++ Lesson9(14_12_23)/UserIdentityApp/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserIdentityApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        private UserManager<IdentityUser> _userManager;
+        public UsersController(UserManager<IdentityUser> userManager){
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View(_userManager.Users);
+        }
+    }
+}
---
+++ Lesson9(14_12_23)/UserIdentityApp/Models/IdentityContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserIdentityApp.Models
+{
+    public class IdentityContext : IdentityDbContext<IdentityUser>{
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options){
+            
+        }
+    }
+}
